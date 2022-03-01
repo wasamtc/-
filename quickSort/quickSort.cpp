1cecca4b694505def4886f70a6850129c5d44dca
wasamtc <2930848926@qq.com>
2022-03-01 17:56:31 +0800

    a better quickSort

--- quickSort/quickSort.cpp
+++ quickSort/quickSort.cpp
@@ -2,13 +2,16 @@
 #include <stdio.h>
 
 int partition(std::vector<int> &nums, int low, int high){
-    int pivot = nums[low];
+    int temp = nums[(low+high)/2];
+    nums[(low+high)/2] = nums[low];
+    nums[low] = temp; //必须要做一个交换，因为后面是与pivotkey进行比较，如果不交换只有nums[low]和nums[high]比较了
+    int pivot = nums[low]; //这儿最好最好进行这样的一个操作，即把pivotkey的选取科学一点，别随便取，随便取效率很低的
 
     while (low < high)
     {
         while(low < high && nums[high] >= pivot) //这儿必须有等号，不然后陷入死循环
             high--;
-        int temp = nums[low];
+        temp = nums[low];
         nums[low] = nums[high];
         nums[high] = temp;
         while(low < high && nums[low] <= pivot)
